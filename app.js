445accac36ea59c96c231fdd895c951db902d024
LeviOP <35459268+LeviOP@users.noreply.github.com>
2018-01-15 10:38:58 -0600

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ bot.on('message' , message => {
 });
 
 // Listner Event: Run on start
-client.on('ready', () => {
+console.on('ready', () => {
     console.log('Bot Launched...')
     // Status
     bot.user.setStatus('Online')
