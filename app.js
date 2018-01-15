d2fb1e140e81f80fb48f9ac383c9557e5138273d
LeviOP <35459268+LeviOP@users.noreply.github.com>
2018-01-15 11:14:47 -0600

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ bot.on('ready', () => {
     // Status
     bot.user.setStatus('dnd')
     //"Playing"
-    bot.setPlayingGame('+help | Bot by Levi')
+    bot.setGame('+help | Bot by Levi')
 });
 // Login
 bot.login('NDAxODI4MDg5NjAxOTgyNDY0.DTv8Xw.m_lQBrcvMVsj-XvHHljCh8gdJuo')
